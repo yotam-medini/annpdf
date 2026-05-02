17fdaf32a22a20a44c2af5d067a264268d0e7891
Yotam Medini <yotam.medini@gmail.com>
2026-05-02 18:09:45 +0300

    debugging Font::Resize

--- annpdf.cpp
+++ annpdf.cpp
@@ -52,8 +52,17 @@ class Font {
   }
   void Resize(int size) {
     if (size_ != size) {
+std::cout << std::format("Resize: {} -> {}\n", size_, size);
       FT_Set_Char_Size(ft_face_, 0, size * 64, 72, 72);
-      hb_ft_font_changed(hb_font_);
+      // hb_ft_font_changed(hb_font_);
+      if (hb_font_) {
+        hb_font_destroy(hb_font_);
+      }
+      hb_font_ = hb_ft_font_create(ft_face_, nullptr);
+      if (c_face_) {
+        cairo_font_face_destroy(c_face_);
+      }
+      c_face_ = cairo_ft_font_face_create_for_ft_face(ft_face_, 0);
       size_ = size;
     }
   }
@@ -444,6 +453,7 @@ void AnnPdf::HarfBuzzShaping(
     hb_buffer_t* hb_buffer,
     const AnnotationText &at,
     const Font &font) {
+std::cerr << std::format("{} text={}\n", __func__, at.text_);
   hb_buffer_add_utf8(hb_buffer, at.text_.c_str(), -1, 0, -1);
   hb_direction_t hb_dir = at.is_ltr_ ? HB_DIRECTION_LTR : HB_DIRECTION_RTL;
   hb_buffer_set_direction(hb_buffer, hb_dir);
@@ -455,6 +465,7 @@ void AnnPdf::HarfBuzzShaping(
 }
 
 void AnnPdf::ApplyFont(cairo_t *cr, const Font &font) {
+  cairo_set_font_face(cr, nullptr); // drop cache
   cairo_set_font_face(cr, font.c_face_);
   cairo_matrix_t font_matrix;
   cairo_matrix_init(&font_matrix, font.size_, 0, 0, -font.size_, 0, 0);
@@ -475,9 +486,12 @@ void AnnPdf::ShowGlyphs(
   double cy = at.xy_[1];
   std::vector<cairo_glyph_t> cairo_glyphs(glyph_count);
 
+std::cerr << std::format("glyph_count={}\n", glyph_count);
   for (unsigned int g = 0; g < glyph_count; ++g) {
       cairo_glyphs[g].index = info[g].codepoint;
       cairo_glyphs[g].x = cx + (pos[g].x_offset / 64.0);
+std::cerr << std::format("g={} x_offset={} x_advance={} x={}\n",
+ g, pos[g].x_offset, pos[g].x_advance, cairo_glyphs[g].x);
       cairo_glyphs[g].y = cy - (pos[g].y_offset / 64.0);
       cx += pos[g].x_advance / 64.0;
       cy += pos[g].y_advance / 64.0;
